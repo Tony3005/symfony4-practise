5ffe816c571a57e351d364640470355f601a9b24
Tony Thiboust <thiboust.tony@gmail.com>
2018-07-01 12:20:21 +0100

    Add Practse controller

---
+++ src/Controller/PractiseController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class PractiseController extends AbstractController 
+{
+    /**
+     * @Route("/practise", name="practise")
+     */
+    public function index(SessionInterface $session, UrlGeneratorInterface $urlGenerator)
+    {
+        $session->set('key', 'value');
+        $url = $urlGenerator->generate('practise');
+
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/PractiseController.php',
+            'url' => $url,
+            'keu' => $session->get('key'),
+        ]);
+    }
+}
