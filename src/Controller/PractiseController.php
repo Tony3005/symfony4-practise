5f4370593f1943e0acb544e55d21c3b947ce7e45
Tony Thiboust <thiboust.tony@gmail.com>
2018-07-29 20:41:01 +0100

    Add cache component

--- src/Controller/PractiseController.php
+++ src/Controller/PractiseController.php
@@ -2,28 +2,13 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
-
 class PractiseController extends AbstractController 
 {
     /**
      * @Route("/practise", name="practise")
      */
-    public function index(SessionInterface $session, UrlGeneratorInterface $urlGenerator)
+    public function index() 
     {
-        $session->set('key', 'value');
-        $url = $urlGenerator->generate('practise');
-
-        // return $this->json([
-        //     'message' => 'Welcome to your new controller!',
-        //     'path' => 'src/Controller/PractiseController.php',
-        //     'url' => $url,
-        //     'keu' => $session->get('key'),
-        // ]);
-
         return $this->render('practise/practise.html.twig');
     }
 }
